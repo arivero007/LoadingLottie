f07756158fee076fe6ed5699273cae7be31bb4d7
alex-Rivero <alex.r@gistek-s.com>
2022-07-06 17:44:33 +0200

    First commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "LoadingLottie",
+    platforms: [
+           .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,16 +16,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "3.2.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LoadingLottie",
-            dependencies: []),
-        .testTarget(
-            name: "LoadingLottieTests",
-            dependencies: ["LoadingLottie"]),
+            dependencies: [.product(name: "Lottie", package: "lottie-ios")]),
     ]
 )
--- Sources/LoadingLottie/LoadingLottie.swift
+++ Sources/LoadingLottie/LoadingLottie.swift
@@ -1,5 +1,4 @@
 public struct LoadingLottie {
-    public private(set) var text = "Hello, World!"
 
     public init() {
     }
