63f6111ba32542200a2a7412301f9e970dda3ae5
alex-Rivero <alex.r@gistek-s.com>
2022-07-06 17:26:19 +0200

    Initial Commit

---
+++ Sources/LoadingLottie/LoadingLottie.swift
@@ -0,0 +1,6 @@
+public struct LoadingLottie {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/LoadingLottieTests/LoadingLottieTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LoadingLottie
+
+final class LoadingLottieTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LoadingLottie().text, "Hello, World!")
+    }
+}
